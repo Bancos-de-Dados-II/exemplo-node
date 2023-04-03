57adeefa041cb4766db1a041c7b5a4f9fbfeb505
Paulo Freitas <paulo.freitas.nt@gmail.com>
2023-04-03 16:59:08 -0300

    Adicionando modelo

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
 
 const sequelize = new Sequelize('aula', 'postgres', 'postgres', {
     host: 'localhost',
     dialect: 'postgres'
 });
 
-conectar();
+const Usuario = sequelize.define('Usuario', {
+    // Model attributes are defined here
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    nome: {
+      type: DataTypes.STRING
+      // allowNull defaults to true
+    }
+  }, {
+    // Other model options go here
+  });
 
-async function conectar(){
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-      } catch (error) {
-        console.error('Unable to connect to the database:', error);
-      }
-}
+  sincronizar();
+
+  async function sincronizar(){
+    await Usuario.sync();
+  }
+
+
+// conectar();
+// async function conectar(){
+//     try {
+//         await sequelize.authenticate();
+//         console.log('Connection has been established successfully.');
+//       } catch (error) {
+//         console.error('Unable to connect to the database:', error);
+//       }
+// }
