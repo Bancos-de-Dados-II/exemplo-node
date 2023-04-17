8415d42bcb8fcda2fe8707bd876453f6dfab599c
Paulo Freitas <paulo.freitas.nt@gmail.com>
2023-04-17 15:33:09 -0300

    Configurando rotas com o express router

--- index.js
+++ index.js
@@ -2,14 +2,9 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 const port = 3000;
-const usuarioController = require('./controllers/UsuarioController');
+const usuarioRouter = require('./routes/UsuarioRouter');
 
-app.get('/usuarios', usuarioController.listarUsuarios);
-app.get('/usuarios/:id', usuarioController.buscarPelaChave);
-app.post('/usuarios', usuarioController.salvarUsuario);
-app.delete('/usuarios/:id', usuarioController.deletarUsuario);
-app.put('/usuarios/:id', usuarioController.atualizarUsuario);
-
+app.use('/usuarios',usuarioRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
---
+++ routes/UsuarioRouter.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+const usuarioController = require('../controllers/UsuarioController');
+
+router.get('/', usuarioController.listarUsuarios);
+router.get('/:id', usuarioController.buscarPelaChave);
+router.post('/', usuarioController.salvarUsuario);
+router.delete('/:id', usuarioController.deletarUsuario);
+router.put('/:id', usuarioController.atualizarUsuario);
+
+module.exports = router;
