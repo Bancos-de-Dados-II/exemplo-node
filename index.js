39ef7fcc59cbefa696d4957ff845e6efad77eb4f
Paulo Freitas <paulo.freitas.nt@gmail.com>
2023-05-29 17:00:27 -0300

    Adicionando router do mongodb

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ app.use(express.json());
 const port = 3000;
 const usuarioRouter = require('./routes/UsuarioRouter');
 const pontoRouter = require('./routes/PontoRouter');
+const AnotacaoRouter = require('./routes/AnotacaoRouter');
 
 const cors = require('cors');
 app.use(cors());
 
 app.use('/usuarios',usuarioRouter);
 app.use('/pontos',pontoRouter);
+app.use('/anotacoes', AnotacaoRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
