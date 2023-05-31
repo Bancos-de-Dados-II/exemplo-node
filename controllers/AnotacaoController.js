540b586d0bad856484268f5cd2c2473b66d637db
Paulo Freitas <paulo.freitas.nt@gmail.com>
2023-05-31 14:07:06 -0300

    Criando método para remover anotação

--- controllers/AnotacaoController.js
+++ controllers/AnotacaoController.js
@@ -1,30 +1,27 @@
 const Anotacao = require('../models/Anotacao');
 
 const listarAnotacoes = async (req,res) => {
-    Anotacao.find({},{_id:false, __v:false}).then(result => {
+    Anotacao.find({},{_id:true, __v:false}).then(result => {
         res.status(200).send(result);
     }).catch(e => res.status(400).send(e));
 }
 
 const buscarPorConteudo = async (req,res) =>{
-    Anotacao.find({$text:{$search:req.params.conteudo}},{_id:false, __v:false}).then(result => {
+    Anotacao.find({$text:{$search:req.params.conteudo}},{_id:true, __v:false}).then(result => {
         res.status(200).send(result);
     }).catch(e => res.status(400).send(e));
 }
 
 const salvarAnotacao = async (req,res) =>{
-    Anotacao.create(req.body).then(result => res.status(200).send(result)).catch(e => res.status(400).send(e));
+    Anotacao.create(req.body).then(result => {res.status(201).send(result)}).catch(e => res.status(400).send(e));
 }
 
-// const deletarUsuario = async (req,res) =>{
-//     const usuario = await Usuario.findByPk(req.params.id);
-//     if(usuario == null){
-//         res.status(404).send('Usuário não encontrado');
-//     }else{
-//         await usuario.destroy();
-//         res.status(200).send(`Usuário ${req.params.id} excluído`);
-//     }
-// }
+const deletarAnotacao = async (req,res) =>{
+    Anotacao.deleteOne({_id:req.params.id}).then(result => {
+        if(result.deletedCount > 0) res.status(200).send('Removido com sucesso');
+        else res.status(404).send('Anotação não encontrada');
+    }).catch(e => res.status(400).send(e));
+}
 
 // const atualizarUsuario = async (req,res) =>{
 //     const usuario = await Usuario.findByPk(req.params.id);
@@ -37,9 +34,4 @@ const salvarAnotacao = async (req,res) =>{
 //     }
 // }
 
-// const sincronizar = async (req,res) =>{
-//     await Usuario.sync();
-//     res.status(200).send('Sincronizado');
-// }
-
-module.exports = {listarAnotacoes, salvarAnotacao, buscarPorConteudo};
+module.exports = {listarAnotacoes, salvarAnotacao, buscarPorConteudo, deletarAnotacao};
